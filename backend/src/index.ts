1d5b0f782ffda6f33457625a9a1401338aa2e3dc
Matheus <matheusp.sampaio@gmail.com>
2025-09-26 13:18:48 -0300

    fix: removendo / no final da url do client frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 const allowedOrigins = [
-    'https://url-shortener-client-indol.vercel.app/',
+    'https://url-shortener-client-indol.vercel.app',
     'http://localhost:5173'
 ];
 
